7674927a6f76ea984446ad4e9a7bc0cac4c6704e
Viktor Bobrov <vbobrov@gmail.com>
2022-11-15 19:30:08 -0500

    switched to different websocket module

--- ws_context_in.py
+++ ws_context_in.py
@@ -1,24 +1,25 @@
-import requests, websocket, ssl
+import requests, websocket, ssl, json
 from base64 import b64encode
 
-r=requests.post(f"https://vb-cl-ise-px1.ciscodemo.net:8910/pxgrid/control/ServiceLookup",
+r=requests.post(f"https://vb-cl-ise-px1.ciscodemo.net:8910/pxgrid/control/ServiceRegister",
     cert=(".pxgrid-client.crt",".pxgrid-client.key"),
     verify=".demo-ca.cer",
     auth=("pxgrid-client","none"),
     json={
-        "name": "com.cisco.ise.session"
+        "name": "com.cisco.endpoint.asset",
+        "properties": {
+            "wsPubsubService": "com.cisco.ise.pubsub",
+            "assetTopic":"/topic/com.cisco.endpoint.asset"
+        }
     }
 )
 r.raise_for_status()
-service_info=r.json()["services"][0]
-session_topic=service_info["properties"]["sessionTopic"]
-pubsub_service=service_info["properties"]["wsPubsubService"]
 r=requests.post(f"https://vb-cl-ise-px1.ciscodemo.net:8910/pxgrid/control/ServiceLookup",
     cert=(".pxgrid-client.crt",".pxgrid-client.key"),
     verify=".demo-ca.cer",
     auth=("pxgrid-client","none"),
     json={
-        "name": pubsub_service
+        "name": "com.cisco.ise.pubsub"
     }
 )
 r.raise_for_status()
@@ -35,19 +36,14 @@ r=requests.post(f"https://vb-cl-ise-px1.ciscodemo.net:8910/pxgrid/control/Access
 )
 r.raise_for_status()
 secret=r.json()["secret"]
-
-def on_open(wsapp):
-    wsapp.send(f"CONNECT\naccept-version:1.2\nhost:{node_name}\n\n\x00",websocket.ABNF.OPCODE_BINARY)
-    wsapp.send(f"SUBSCRIBE\ndestination:{session_topic}\nid:python\n\n\x00",websocket.ABNF.OPCODE_BINARY)
-
-def on_message(wsapp,message):
-    print(message.decode())
-
 ssl_context=ssl.create_default_context()
 ssl_context.load_verify_locations(cafile=".demo-ca.cer")
-wsapp=websocket.WebSocketApp(ws_url,
-    on_message=on_message,
-    on_open=on_open,
+ws=websocket.create_connection(ws_url,
+    sslopt={"context": ssl_context},
     header={"Authorization": "Basic "+b64encode((f"pxgrid-client:{secret}").encode()).decode()}
 )
-wsapp.run_forever(sslopt={"context": ssl_context})
+with open("endpoint.json","r") as f:
+    endpoint=json.dumps(json.loads(f.read()))
+ws.send(f"CONNECT\naccept-version:1.2\nhost:{node_name}\n\n\x00",websocket.ABNF.OPCODE_BINARY)
+ws.send(f"SEND\ndestination:/topic/com.cisco.endpoint.asset\ncontent-length:{len(endpoint)}\n\n{endpoint}\x00".encode("utf-8"),websocket.ABNF.OPCODE_BINARY)
+ws.close()
